0dd3d0f2f5a052d19bfb6c55bde37aabda0ddc59
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-06-20 02:30:59 +0100

    setup config vars for db

--- build/util/database.js
+++ build/util/database.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+const sequelize = new sequelize_1.Sequelize(`${process.env.DB}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
     dialect: 'mysql',
     host: `${process.env.DB_HOST}`
 });
--- src/util/database.ts
+++ src/util/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+const sequelize = new Sequelize(`${process.env.DB}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
   dialect: 'mysql',
   host: `${process.env.DB_HOST}`
 });
