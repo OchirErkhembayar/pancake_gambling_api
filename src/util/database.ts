04319b3cfad102354e2b0ec1df3a8f36f57d51f1
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-06-20 02:24:31 +0100

    util db file

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.MYSQL_PW}`, {
+const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
   dialect: 'mysql',
   host: `${process.env.DB_HOST}`
 });
